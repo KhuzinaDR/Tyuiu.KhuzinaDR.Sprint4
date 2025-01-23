83e11bc78d9e03b1384368324d7c5400365cb859
angeldrip9919 <i61angel>
2025-01-24 02:32:18 +0500

    Task6

---
+++ Tyuiu.KhuzinaDR.Sprint4.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+namespace Tyuiu.KhuzinaDR.Sprint4.Task6.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.KhuzinaDR.Sprint4.Task6.V13/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KhuzinaDR.Sprint4.Task6.V13.Lib;
+namespace Tyuiu.KhuzinaDR.Sprint4.Task6.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнила: Хузина Д. Р. | СМАРТб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Хузина Диана Ранидовна | СМАРТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string[] carBrands = { "Ford", "Toyota", "Honda", "Chevrolet", "Mercedes", "BMW", "Audi" };
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(carBrands);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
