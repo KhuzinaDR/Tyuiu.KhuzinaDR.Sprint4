b089a65b87b3be65a68c6ac9b259f5a4e665d703
angeldrip9919 <i61angel>
2025-01-24 00:44:04 +0500

    Task1

---
+++ Tyuiu.KhuzinaDR.Sprint4.Task1.V18.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.KhuzinaDR.Sprint4.Task1.V18.Lib;
+namespace Tyuiu.KhuzinaDR.Sprint4.Task1.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 9, 8, 7, 6, 5 };
+
+            int wait = 21;
+            int res = ds.Calculate(array);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
